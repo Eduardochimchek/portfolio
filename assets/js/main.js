1914bae2854813d39ab22afd10e1450a3732d552
Eduardo Chimchek Jeronimo <eduardochimcheck@gmail.com>
2024-02-16 14:21:30 -0300

    Melhorias

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,47 +9,20 @@ initScrollReveal();
 typeWrite(document.querySelector(".typewriter"));
 
 hoverChangeExperience(
-  ".metaspark",
-  `Trabalhei como criador de filtros para o Instagram. Nessa função, 
-  fui responsável por desenvolver e implementar conceitos de filtros para imagens e vídeos que fossem criativos, 
-  atraentes e relevantes para o público do Instagram. Utilizei habilidades avançadas em programas de design, bem como 
-  conhecimentos técnicos em linguagens de programação, como Spark AR e JavaScript. Para garantir que os filtros
-  atendessem aos objetivos do cliente e melhorassem a experiência do usuário`,
-  "Meta Spark Creator",
-  "Meta",
-  "Nov 2018 - Atualmente"
-);
-
-hoverChangeExperience(
-  ".takepic",
-  `Trabalho gerenciando uma equipe de editores de fotografia, garantindo a entrega de imagens de alta 
-  qualidade dentro dos prazos estabelecidos. Desenvolvendo e implementando processos eficientes de edição de fotografia, 
-  aumentando a produtividade e eficiência da equipe. Colaborando com outros departamentos para garantir que as imagens 
-  atendam aos requisitos de qualidade e de marca da empresa. `,
-  "Coordenador de Edição",
-  "TakePic",
-  "Mar 2023 - Atualmente"
-);
-
-hoverChangeExperience(
-  ".advertiz",
-  `Atuei como Social Media e Designer na AdvertiZ., uma empresa focada em criar conteúdo de programação para a comunidade de 
-  desenvolvedores através das redes sociais, com a criação de estratégias de marketing digital para aumentar a presença online da empresa e 
-  divulgar os conteúdos produzidos pela equipe. Também fui responsável por criar conteúdos visuais atraentes para as redes sociais, 
-  incluindo posts, imagens e vídeos.`,
-  "Social Media e Designer",
-  "AdvertiZ.",
-  "Jun 2021 - Jan 2022"
-);
-
-hoverChangeExperience(
-  ".painhas",
-  `Trabalhei como Técnico de Transmissão em uma empresa de mídia, onde fui responsável por garantir que o 
-  equipamento de transmissão ao vivo fosse configurado e operado de forma adequada, permitindo que os eventos fossem transmitidos 
-  com qualidade e sem interrupções, a fim de proporcionar uma experiência positiva aos espectadores.`,
-  "Técnico de Transmissão",
-  "Painhas",
-  "Ago 2022 - Outubro 2022"
+  ".Agrosys",
+  `Durante meu primeiro estágio, tive a oportunidade de adquirir valiosas lições sobre
+  comportamento profissional e compreender o funcionamento da engrenagem na minha área.
+  Durante seis meses de estágio, mergulhei no aprendizado e na execução de tarefas,
+  adaptando minhas habilidades para ingressar na área de produto.
+  Foi nesse período que aprendi e apliquei os padrões da empresa, os quais continuei a utilizar
+  após ser efetivado.
+  Posteriormente, fui efetivado no departamento de tecnologia da empresa, onde atuei como
+  desenvolvedor full-stack.
+  Foi nesse ambiente que adquiri um vasto conhecimento em template (HTML, CSS, JavaScript),
+  contribuindo com soluções tanto para uso interno quanto externo da empresa.`,
+  "Agrosys Tecnologia",
+  "Agrosys",
+  "Mar 2021 - Atualmente"
 );
 
 hoverChangeDescription(
@@ -65,38 +38,10 @@ hoverChangeDescription(
   "JavaScript é uma linguagem de programação que permite a você implementar elementos dinâmicos em páginas web."
 );
 hoverChangeDescription(
-  ".sass",
-  "Sass é um pré-processador CSS que adiciona alguns recursos que não estão disponíveis de forma nativa."
-);
-hoverChangeDescription(
   ".react",
   "React é uma biblioteca JavaScript com foco em criar interfaces de usuário de forma componetizada."
 );
 hoverChangeDescription(
-  ".next",
-  "Next.js é uma estrutura da web que permite funcionalidades como renderização do lado do servidor e geração de sites estáticos para web baseados em React."
-);
-hoverChangeDescription(
-  ".styled",
-  "O Spark AR é uma linguagem de programação usada para criar efeitos de realidade aumentada (AR) para as plataformas da Meta. A linguagem é baseada em JavaScript. Os desenvolvedores utilizam o Spark AR para criar e personalizar efeitos de AR, como modelagem 3D, animação, rastreamento de objetos e reconhecimento facial."
-);
-hoverChangeDescription(
-  ".tailwind",
-  "Tailwind CSS é um framework CSS que nos fornece classes utilitárias com o propósito de estilizar as páginas."
-);
-hoverChangeDescription(
   ".typescript",
   "TypeScript é um superset do JavaScript que incluir recursos que não estão presentes na linguagem de forma nativa, além de torná-la estática."
-);
-hoverChangeDescription(
-  ".radix",
-  "Radix é uma biblioteca que fornece componentes acessíveis e sem estilos para a criação de aplicação em React."
-);
-hoverChangeDescription(
-  ".cypress",
-  "Cypress é um framework para automação de testes end-to-end, onde atualmente usa a linguagem JavaScript."
-);
-hoverChangeDescription(
-  ".storybook",
-  "Storybook é uma ferramenta que tem como objetivo documentar e testar os componentes da aplicação."
 );
